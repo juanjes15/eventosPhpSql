671a07c3ebeccaf07b088e535d002746f5b1e94c
Juan Jeisson <juanjes15@gmail.com>
2023-06-08 16:56:40 -0500

    space->tab

--- asistentes.php
+++ asistentes.php
@@ -2,97 +2,97 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <title>Sistema de Gestion de Asistencia a Eventos</title>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+	<title>Sistema de Gestion de Asistencia a Eventos</title>
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg" style="background-color: #0998ff8a;">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="index.php"><strong>Inicio</strong></a>
-            <a class="navbar-brand" href="eventos.php"><strong>Eventos</strong></a>
-            <a class="navbar-brand" href="ubicaciones.php"><strong>Ubicaciones</strong></a>
-            <a class="navbar-brand" href="asistentes.php"><strong>Asistentes</strong></a>
-            <a class="navbar-brand" href="asistencia.php"><strong>Asistencia</strong></a>
-        </div>
-    </nav><br>
-    <div class="container text-center p-2 rounded-5" style="background-color: #BFD4E4;">
-        <div class="row py-3">
-            <div class="col">
-                <h1 class="h1">Asistentes</h1>
-            </div>
-        </div>
-        <div class="row py-3">
-            <form method="post">
-                <div class="row justify-content-center py-2">
-                    <div class="col-1">
-                        <label for="nombre" class="col-form-label">Nombre:</label>
-                    </div>
-                    <div class="col-4">
-                        <input type="text" id="nombre" class="form-control">
-                    </div>
-                </div>
-                <div class="row justify-content-center py-2">
-                    <div class="col-1">
-                        <label for="apellido" class="col-form-label">Apellido:</label>
-                    </div>
-                    <div class="col-4">
-                        <input type="text" id="apellido" class="form-control">
-                    </div>
-                </div>
-                <div class="row justify-content-center py-2">
-                    <div class="col-1">
-                        <label for="correo" class="col-form-label">Correo:</label>
-                    </div>
-                    <div class="col-4">
-                        <input type="email" id="correo" class="form-control">
-                    </div>
-                </div>
-                <div class="row justify-content-center py-2">
-                    <div class="col-2">
-                        <button type="submit" class="btn btn-info">Crear</button>
-                    </div>
-                    <div class="col-2">
-                        <button type="submit" class="btn btn-info">Buscar</button>
-                    </div>
-                    <div class="col-2">
-                        <button type="submit" class="btn btn-info">Actualizar</button>
-                    </div>
-                    <div class="col-2">
-                        <button type="submit" class="btn btn-info">Eliminar</button>
-                    </div>
-                </div>
-            </form>
-        </div>
-        <div class="row justify-content-center py-3">
-            <div class="col-8">
-                <table class="table table-light table-sm table-hover table-bordered">
-                    <thead class="table-primary">
-                        <tr>
-                            <th scope="col">Nombre</th>
-                            <th scope="col">Apellido</th>
-                            <th scope="col">Correo</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>Manizales</td>
-                        </tr>
-                        <tr>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>Pereira</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
+	<nav class="navbar navbar-expand-lg" style="background-color: #0998ff8a;">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="index.php"><strong>Inicio</strong></a>
+			<a class="navbar-brand" href="eventos.php"><strong>Eventos</strong></a>
+			<a class="navbar-brand" href="ubicaciones.php"><strong>Ubicaciones</strong></a>
+			<a class="navbar-brand" href="asistentes.php"><strong>Asistentes</strong></a>
+			<a class="navbar-brand" href="asistencia.php"><strong>Asistencia</strong></a>
+		</div>
+	</nav><br>
+	<div class="container text-center p-2 rounded-5" style="background-color: #BFD4E4;">
+		<div class="row py-3">
+			<div class="col">
+				<h1 class="h1">Asistentes</h1>
+			</div>
+		</div>
+		<div class="row py-3">
+			<form method="post">
+				<div class="row justify-content-center py-2">
+					<div class="col-1">
+						<label for="nombre" class="col-form-label">Nombre:</label>
+					</div>
+					<div class="col-4">
+						<input type="text" id="nombre" class="form-control">
+					</div>
+				</div>
+				<div class="row justify-content-center py-2">
+					<div class="col-1">
+						<label for="apellido" class="col-form-label">Apellido:</label>
+					</div>
+					<div class="col-4">
+						<input type="text" id="apellido" class="form-control">
+					</div>
+				</div>
+				<div class="row justify-content-center py-2">
+					<div class="col-1">
+						<label for="correo" class="col-form-label">Correo:</label>
+					</div>
+					<div class="col-4">
+						<input type="email" id="correo" class="form-control">
+					</div>
+				</div>
+				<div class="row justify-content-center py-2">
+					<div class="col-2">
+						<button type="submit" class="btn btn-info">Crear</button>
+					</div>
+					<div class="col-2">
+						<button type="submit" class="btn btn-info">Buscar</button>
+					</div>
+					<div class="col-2">
+						<button type="submit" class="btn btn-info">Actualizar</button>
+					</div>
+					<div class="col-2">
+						<button type="submit" class="btn btn-info">Eliminar</button>
+					</div>
+				</div>
+			</form>
+		</div>
+		<div class="row justify-content-center py-3">
+			<div class="col-8">
+				<table class="table table-light table-sm table-hover table-bordered">
+					<thead class="table-primary">
+						<tr>
+							<th scope="col">Nombre</th>
+							<th scope="col">Apellido</th>
+							<th scope="col">Correo</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Mark</td>
+							<td>Otto</td>
+							<td>Manizales</td>
+						</tr>
+						<tr>
+							<td>Jacob</td>
+							<td>Thornton</td>
+							<td>Pereira</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
 </body>
 
 </html>
--- database.php
+++ database.php
@@ -1,26 +1,28 @@
 <?php
 
-class Database {
-    private $host = 'localhost';
-    private $db_name = 'eventos';
-    private $username = 'root';
-    private $password = '';
-    private $conn;
+class Database
+{
+	private $host = 'localhost';
+	private $db_name = 'eventos';
+	private $username = 'root';
+	private $password = '';
+	private $conn;
 
-    public function getConnection() {
-        $this->conn = null;
+	public function getConnection()
+	{
+		$this->conn = null;
 
-        try {
-            $this->conn = new PDO(
-                "mysql:host={$this->host};dbname={$this->db_name}",
-                $this->username,
-                $this->password
-            );
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (PDOException $exception) {
-            echo 'Error de conexión: ' . $exception->getMessage();
-        }
+		try {
+			$this->conn = new PDO(
+				"mysql:host={$this->host};dbname={$this->db_name}",
+				$this->username,
+				$this->password
+			);
+			$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		} catch (PDOException $exception) {
+			echo 'Error de conexión: ' . $exception->getMessage();
+		}
 
-        return $this->conn;
-    }
+		return $this->conn;
+	}
 }
